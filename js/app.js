3a119feb402739b97a6b0c9a884c809bcd8b34f3
laloehm <laloehm1@gmail.com>
2020-11-05 12:08:57 -0600

    Se realizo el menu

--- js/app.js
+++ js/app.js
@@ -114,10 +114,23 @@ icon.addEventListener('click', function () {
     $(span).toggleClass('span-active');
 })
 
-
-
-
-
-
-console.log(icon)
+/* Smooth scrolling para anclas */
+$("a.smooth").on("click", function (e) {
+    e.preventDefault();
+    var $link = $(this);
+    var anchor = $link.attr("href");
+    $("html, body").stop().animate({
+        scrollTop: $(anchor).offset().top - 100 + "px"
+    }, 1000);
+});
+// Funcion para barra de Menu
+$(window).scroll(function () {
+    let header = $(".navbar");
+    if ($(".navbar").offset().top > 500) {
+        $(header).css("background", "#fff");
+    } else {
+        // $(".navbar").addClass("navbar-expand-lg");
+        $(header).css("background", "transparent");
+    }
+});
 
