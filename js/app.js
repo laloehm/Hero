aab8ec49514cabc0c034bc370c30e3abf1e84b4a
laloehm <laloehm1@gmail.com>
2020-11-19 19:14:58 -0600

    Mas avances en la version mobile

--- js/app.js
+++ js/app.js
@@ -20,90 +20,7 @@
 //     }
 // })
 
-//global variables
-var items = [];
-var startItem = 1;
-var position = 0;
-var itemCount = $('.box-carousel .carousel li.items').length;
-var leftpos = itemCount;
-var resetCount = itemCount;
 
-//unused: gather text inside items class
-$('.box-carousel li.items').each(function (index) {
-    items[index] = $(this).text();
-});
-
-//swap images function
-function swapp(action) {
-    var direction = action;
-
-    //moving carousel backwards
-    if (direction == 'counter-clockwise') {
-        var leftitem = $('.box-carousel .left-pos').attr('id') - 1;
-        if (leftitem == 0) {
-            leftitem = itemCount;
-        }
-
-        $('.box-carousel .right-pos').removeClass('right-pos').addClass('back-pos');
-        $('.box-carousel .main-pos').removeClass('main-pos').addClass('right-pos');
-        $('.box-carousel .left-pos').removeClass('left-pos').addClass('main-pos');
-        $('#' + leftitem + '').removeClass('back-pos').addClass('left-pos');
-
-        startItem--;
-        if (startItem < 1) {
-            startItem = itemCount;
-        }
-    }
-
-    //moving carousel forward
-    if (direction == 'clockwise' || direction == '' || direction == null) {
-        function pos(positionvalue) {
-            if (positionvalue != 'leftposition') {
-                //increment image list id
-                position++;
-
-                //if final result is greater than image count, reset position.
-                if ((startItem + position) > resetCount) {
-                    position = 1 - startItem;
-                }
-            }
-
-            //setting the left positioned item
-            if (positionvalue == 'leftposition') {
-                //left positioned image should always be one left than main positioned image.
-                position = startItem - 1;
-
-                //reset last image in list to left position if first image is in main position
-                if (position < 1) {
-                    position = itemCount;
-                }
-            }
-
-            return position;
-        }
-
-        $('#' + startItem + '').removeClass('main-pos').addClass('left-pos');
-        $('#' + (startItem + pos()) + '').removeClass('right-pos').addClass('main-pos');
-        $('#' + (startItem + pos()) + '').removeClass('back-pos').addClass('right-pos');
-        $('#' + pos('leftposition') + '').removeClass('left-pos').addClass('back-pos');
-
-        startItem++;
-        position = 0;
-        if (startItem > itemCount) {
-            startItem = 1;
-        }
-    }
-}
-
-//next button click function
-$('#next').click(function () {
-    swapp('clockwise');
-});
-
-//prev button click function
-$('#prev').click(function () {
-    swapp('counter-clockwise');
-});
 
 let icon = document.querySelector('#icon-menu');
 let menu = document.querySelector('#menu');
@@ -165,5 +82,10 @@ if (URLactual == 'http://127.0.0.1:5500/heroes-hero.html') {
         }
     });
 }
+
+let parrafo  = document.querySelector('.carousel-item p');
+primera = parrafo.split(' . ')[0];
+
+console.log(primera)
 
 
