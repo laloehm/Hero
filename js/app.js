0723c961c060c5241d3dcd55c2b913367bdd9e6d
laloehm <laloehm1@gmail.com>
2020-11-22 17:48:43 -0600

    Se agregaron causas

--- js/app.js
+++ js/app.js
@@ -1,27 +1,3 @@
-// $('.owl-carousel').owlCarousel({
-//     loop:true,
-//     margin: 10,
-//     nav: true,
-//     dots: false,
-//     navText:
-//        ["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
-//     responsive:{
-//         0:{
-//             items: 1,
-//             nav: true
-//         },
-//         600:{
-//             items: 3,
-//             nav: true
-//         },
-//         1000:{
-//             items: 4
-//         }
-//     }
-// })
-
-
-
 let icon = document.querySelector('#icon-menu');
 let menu = document.querySelector('#menu');
 let span = $('.icon span');
@@ -56,36 +32,81 @@ $(window).scroll(function () {
 var URLactual = window.location;
 
 if (URLactual == 'http://127.0.0.1:5500/heroes-hero.html') {
-  
+
     let icon = document.querySelectorAll('#icon-menu span');
     icon.forEach(element => {
         element.style.background = "#fff"
     });
 
     let links = document.querySelectorAll('.smooth');
-        links.forEach(element => {
+    links.forEach(element => {
         element.style.color = "#f9f9f9"
-        })
-    
+    })
+
     let logo = document.querySelector('.logo').style.display = "block";
     let barra = document.querySelector('.heroes').style.background = "#000"
-    
-    $(window).scroll(function () {  
+
+    $(window).scroll(function () {
         if ($(".navbar").offset().top > 500) {
             icon.forEach(element => {
-            element.style.background = "#000"
-        });
+                element.style.background = "#000"
+            });
         } else {
             icon.forEach(element => {
                 element.style.background = "#fff";
-        });        
+            });
         }
     });
 }
 
-let parrafo  = document.querySelector('.carousel-item p');
-primera = parrafo.split(' . ')[0];
+// Carousel causas
 
-console.log(primera)
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 2,
+            nav: true,
+            dots: false
+        },
+        1000: {
+            items: 3,
+            nav: true,
+            dots: false
+        }
+    }
+})
 
+let next = $('.owl-next');
+let prev = $('.owl-prev');
+let activo = $('.owl-item.active');
+$(activo).css({ 'transform': 'scale(.85)', 'opacity': .2 });
+$(activo[1]).css({'transform':'scale(1)', 'opacity': 1 });
+let name = $('.owl-item.active .name');
+$(name[1]).css('display', 'flex');
 
+$(next).on('click', function () {
+    let activos = $('.owl-item.active');
+    let name = $('.owl-item.active .name');
+    $(activos).css({'transform' : 'scale(.85)','opacity' : .2});
+    $(activos[1]).css({'transform':'scale(1)', 'opacity': 1 });
+    $(name).css('display', 'none')
+    $(name[1]).css('display', 'flex');
+    console.log(activos)
+});
+$(prev).on('click', function () {
+    let activos = $('.owl-item.active');
+    let name = $('.owl-item.active .name');
+    $(activos).css('transform', 'scale(.85)');
+    $(activos[1]).css({'transform':'scale(1)', 'opacity': 1 });
+    $(name).css('display', 'none')
+    $(name[1]).css('display', 'flex');
+    console.log(activos)
+})
+{}
